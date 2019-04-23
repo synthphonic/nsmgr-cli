afaba9ed3855a4e49198cc0ed57a7a32adb316a7
Shah Z. S <>
2019-04-23 15:23:01 +0800

    more tests

--- src/cmdline-parser-test/Options.cs
+++ src/cmdline-parser-test/Options.cs
@@ -1,51 +1,23 @@
-﻿using System.Collections.Generic;
-using CommandLine;
+﻿using CommandLine;
 
 namespace cmdline_parser_test
 {
-    class Options
-    {
-        [Option("help",
-            Default = true,
-            Required = false,
-            HelpText = "")]
-        public bool Help { get; set; }
+	[Verb("add", HelpText = "Add file contents to index.")]
+	class AddOptions
+	{
+		[Option('t', Required =true, HelpText = "The t option")]
+		public bool Truncate { get; set; }
+	}
 
-        [Option("path",
-            Default = false,
-            Required = false,
-            HelpText = "The path where the .sln resides")]
-        public bool SolutionPath { get; set; }
+	[Verb("commit", HelpText = "Record changes to the repository.")]
+	class CommitOptions
+	{
 
-        [Option("config",
-            Default = false,
-            Required = false,
-            HelpText = "The configuration file to use")]
-        public bool Config { get; set; }
-    }
+	}
 
-    class Options11
-    {
-        [Option('r', "read", Required = false, HelpText = "Input files to be processed.")]
-        public IEnumerable<string> InputFiles { get; set; }
+	[Verb("clone", HelpText = "Clone a repository into a new directory.")]
+	class CloneOptions
+	{
 
-        // Omitting long name, defaults to name of property, ie "--verbose"
-        [Option(
-          Default = false,
-          HelpText = "Prints all messages to standard output.")]
-        public bool Verbose { get; set; }
-
-        [Option("stdin",
-          Default = false,
-          HelpText = "Read from stdin")] 
-        public bool stdin { get; set; }
-
-        [Value(0, MetaName = "offset", HelpText = "File offset.")]
-        public long? Offset { get; set; }
-    }
-    public class Options2
-    {
-        [Option('v', "verbose", Required = true, HelpText = "Set output to verbose messages.")]
-        public bool Verbose { get; set; }
-    }
+	}
 }
--- src/cmdline-parser-test/Program.cs
+++ src/cmdline-parser-test/Program.cs
@@ -1,62 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using CommandLine;
 
 namespace cmdline_parser_test
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            CommandLine.Parser.Default.ParseArguments<Options>(args)
-              .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
-              .WithNotParsed<Options>((errs) => HandleParseError(errs));
-        }
-
-        private static void HandleParseError(IEnumerable<Error> errs)
-        {
-            Console.WriteLine("Handle error");
-        }
-
-        private static void RunOptionsAndReturnExitCode(Options opts)
-        {
-            Console.WriteLine($"run the option {opts.Help}");
-        }
-
-        //static void Main2(string[] args)
-        //{
-        //    Parser.Default.ParseArguments<Options>(args)
-        //                       .WithParsed<Options>(o =>
-        //                       {
-        //                           if (o.Verbose)
-        //                           {
-        //                               Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
-        //                               Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-        //                           }
-        //                           else
-        //                           {
-        //                               Console.WriteLine($"Current Arguments: -v {o.Verbose}");
-        //                               Console.WriteLine("Quick Start Example!");
-        //                           }
-        //                       });
-        //}
-
-        static void MainOriginal(string[] args)
-        {
-            //
-            // (1) default options
-            //
-            //var result = Parser.Default.ParseArguments<Options>(args);
-
-            //
-            // or (2) build and configure instance
-            //
-            var parser = new Parser(with => with.EnableDashDash = true);
-            var result = parser.ParseArguments<Options>(args);
-
-            Console.WriteLine("Hello World!");
-
-
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Parser.Default.ParseArguments<AddOptions, CommitOptions, CloneOptions>(args)
+				.WithParsed<AddOptions>((obj) =>
+				{
+					Console.WriteLine($"AddOptions Truncate : {obj.Truncate}");
+				})
+				.WithParsed<CommitOptions>((obj) =>
+				{
+					Console.WriteLine("CommitOptions");
+				})
+				.WithParsed<CloneOptions>((obj) =>
+				{
+					Console.WriteLine("CloneOptions");
+				})
+				.WithNotParsed(errs =>
+				{
+					Console.WriteLine("error");
+				});
+		}
+	}
 }
