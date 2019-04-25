5c118978f59f91ac2991a71332a28a0063c11ad0
Shah Z. S <shahzshafie@outlook.com>
2019-04-25 10:22:38 +0800

    R&D

--- src/nugetpackageupdater/Core/FileReaders/SolutionFileReader.cs
+++ src/nugetpackageupdater/Core/FileReaders/SolutionFileReader.cs
@@ -1,37 +1,68 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using SolutionNugetPackagesUpdater.Abstraction;
 
 namespace SolutionNugetPackagesUpdater.Core.FileReaders
 {
-	public class SolutionFileReader : IFileReader
-	{
-		private string _file;
+    public class SolutionFileReader : IFileReader
+    {
+        private string _file;
 
-		public object Read(string file)
-		{
-			_file = file;
+        public object Read(string file)
+        {
+            _file = file;
 
-			ReadSolutionFile();
+            return ReadSolutionFile();
+        }
 
-			return null;
-		}
+        private IEnumerable<string> ReadSolutionFile()
+        {
+            var fileContents = new List<string>();
 
-		private void ReadSolutionFile()
-		{
-			var fileName = Path.GetFileName(_file);
+            using (var sr = new StreamReader(_file,Encoding.UTF8))
+            {
+                while (string.IsNullOrEmpty(sr.ReadLine()))
+                {
+                }
 
-			var fileContents = new List<string>();
-			using (var fs = File.OpenRead(_file))
-			{
-				using (var sr = new StreamReader(fs))
-				{
-					fileContents.Add(sr.ReadLine());
-				}
+                var line = sr.ReadLine();
+                while (!string.IsNullOrEmpty(line))
+                {
+                    fileContents.Add(line);
+                    line = sr.ReadLine();
+                }
 
-				fs.Flush();
-				fs.Close();
-			}
-		}
-	}
+                sr.Close();
+            }
+
+            return fileContents;
+        }
+
+        private IEnumerable<string> ReadSolutionFile2()
+        {
+            var fileName = Path.GetFileName(_file);
+
+            var fileContents = new List<string>();
+            using (var fs = File.OpenRead(_file))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var s = sr.ReadLine();
+                    s = sr.ReadLine();
+                    while (!string.IsNullOrWhiteSpace(s))
+                    {
+                        fileContents.Add(s);
+                        s = sr.ReadLine();
+                    }
+                    
+                    sr.Close();
+                }
+
+                fs.Close();
+            }
+
+            return fileContents;
+        }
+    }
 }
--- src/nugetpackageupdater/Core/Services/FindConflictService.cs
+++ src/nugetpackageupdater/Core/Services/FindConflictService.cs
@@ -1,20 +1,39 @@
-﻿using SolutionNugetPackagesUpdater.Core.FileReaders;
+﻿using System.Collections.Generic;
+using System.Linq;
+using SolutionNugetPackagesUpdater.Core.FileReaders;
+using SolutionNugetPackagesUpdater.Core.Models;
 
 namespace SolutionNugetPackagesUpdater.Core.Services
 {
-	public class FindConflictService
-	{
-		private string _fileName;
+    public class FindConflictService
+    {
+        private string _fileName;
+        private IList<SolutionProjectInfo> _spiList;
 
-		public FindConflictService(string fileName)
-		{
-			_fileName = fileName;
-		}
+        public FindConflictService(string fileName)
+        {
+            _fileName = fileName;
+            _spiList = new List<SolutionProjectInfo>();
+        }
 
-		public void Run()
-		{
-			var slnFileReader = new SolutionFileReader();
-			slnFileReader.Read(_fileName);
-		}
-	}
+        public void Run()
+        {
+            var slnFileReader = new SolutionFileReader();
+            var fileContents = slnFileReader.Read(_fileName) as IEnumerable<string>;
+
+            var f = new List<string>(fileContents);
+            var searchResults = f.Where(x => x.Contains("Project(")).ToList();
+
+            foreach (var item in searchResults)
+            {
+                Extract(item);
+            }
+        }
+
+        private void Extract(string data)
+        {
+            var spi = SolutionProjectInfo.Extract(data);
+            _spiList.Add(spi);
+        }
+    }
 }
