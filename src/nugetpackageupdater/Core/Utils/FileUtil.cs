0bffcfc050c7ddf6710b2369b841232c5fed34b9
Shah Z. S <>
2019-04-29 02:02:21 +0800

    GetFullPath,  PathCombine

--- src/nugetpackageupdater/Core/Utils/FileUtil.cs
+++ src/nugetpackageupdater/Core/Utils/FileUtil.cs
@@ -1,10 +1,17 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace SolutionNugetPackagesUpdater.Core.Utils
 {
 	public static class FileUtil
 	{
+		internal static string GetFullPath(string fullFilePath)
+		{
+			var fullDirectory = Path.GetDirectoryName(fullFilePath);
+			return fullDirectory;
+		}
+
 		internal static string GetFullPath(Assembly asm)
 		{
 			var fullDirectory = Path.GetDirectoryName(asm.Location);
@@ -14,6 +21,11 @@ namespace SolutionNugetPackagesUpdater.Core.Utils
 		internal static bool IsFileExists(string fileName)
 		{
 			return File.Exists(fileName);
+		}
+
+		internal static string PathCombine(string path1, string path2)
+		{
+			return Path.Combine(path1, path2);
 		}
 	}
 }
