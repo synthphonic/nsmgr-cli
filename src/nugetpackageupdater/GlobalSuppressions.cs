1710980171f39772f190bf05b438f940fea3d80b
Shah Z. S <>
2019-05-15 08:56:29 +0800

    include global suppression file

---
+++ src/nugetpackageupdater/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0155:Finds issues with format strings", Justification = "<Pending>", Scope = "member", Target = "~M:SolutionNugetPackagesUpdater.Core.Services.FindConflictService.WriteOutput(System.Collections.Generic.Dictionary{System.String,System.Collections.Generic.IList{SolutionNugetPackagesUpdater.Core.Models.NugetPackageReferenceExtended}})")]
+
