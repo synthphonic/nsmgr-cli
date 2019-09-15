03e530968713e66044b3503e1256055d27a4a98f
Shah Z. S <shahzshafie@outlook.com>
2019-09-16 00:08:40 +0800

    #22
    - Throw exception if logger is null
    - Log file is save to desktop folder

--- src/Nautilus.Cli.Core/Logging/LoggingManager.cs
+++ src/Nautilus.Cli.Core/Logging/LoggingManager.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
+using Nautilus.Cli.Core.Exceptions;
 using Serilog;
 using Serilog.Core;
+using static System.Environment;
 
 namespace Nautilus.Cli.Core.Logging
 {
@@ -10,69 +12,94 @@ namespace Nautilus.Cli.Core.Logging
 		private static readonly LoggingManager _instance = new LoggingManager();
 		private Logger _logger;
 		private string _fileName;
+		private string _fullFileName;
+		private SpecialFolder _desktopSpecialFolder = SpecialFolder.Desktop;
 
 		public static LoggingManager Instance
 		{
 			get { return _instance; }
 		}
 
+		/// <summary>
+		/// Initializes the log instance
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <param name="deleteExistingFile"></param>
 		public void Initialize(string fileName, bool deleteExistingFile)
 		{
 			if (_logger != null)
 				return;
 
+			var writeToFolder = GetFolderPath(_desktopSpecialFolder);
+			_fileName = fileName;
+			_fullFileName = Path.Combine(writeToFolder, _fileName);
+
 			if (deleteExistingFile)
 			{
-				if (File.Exists(fileName))
+				if (File.Exists(_fullFileName))
 				{
-					File.Delete(fileName);
+					File.Delete(_fullFileName);
 				}
-
 			}
 
-			_fileName = fileName;
-
 			_logger = new LoggerConfiguration()
-				.WriteTo.File(_fileName, shared: true, fileSizeLimitBytes: null)
+				.WriteTo.File(_fullFileName, shared: true, fileSizeLimitBytes: null)
 				.CreateLogger();
 		}
 
 		public void WriteInformation(string message)
 		{
+			ThrowExceptionifLoggerIsNull();
 			_logger.Write(Serilog.Events.LogEventLevel.Information, message);
 		}
 
 		public void WriteWarning(string message)
 		{
+			ThrowExceptionifLoggerIsNull();
 			_logger.Write(Serilog.Events.LogEventLevel.Warning, message);
 		}
 
 		public void WriteError(string message)
 		{
+			ThrowExceptionifLoggerIsNull();
 			_logger.Write(Serilog.Events.LogEventLevel.Error, message);
 		}
 
 		public void WriteFatal(string message)
 		{
+			ThrowExceptionifLoggerIsNull();
 			_logger.Write(Serilog.Events.LogEventLevel.Fatal, message);
 		}
 
 		public void WriteVerbose(string message)
 		{
+			ThrowExceptionifLoggerIsNull();
 			_logger.Write(Serilog.Events.LogEventLevel.Verbose, message);
 		}
 
 		public void WriteVerbose(Exception ex, string message)
 		{
+			ThrowExceptionifLoggerIsNull();
 			_logger.Write(Serilog.Events.LogEventLevel.Verbose, ex, message);
 		}
 
-		public void Disable()
+		/// <summary>
+		/// Closes the log file and dispose any connection to the file log
+		/// </summary>
+		public void Close()
 		{
 			if (_logger != null)
 			{
 				_logger.Dispose();
 				_logger = null;
+			}
+		}
+
+		private void ThrowExceptionifLoggerIsNull()
+		{
+			if (_logger == null)
+			{
+				throw new LoggingException("Log manager was not initialized. Make sure to call Initialize() first");
 			}
 		}
 	}
