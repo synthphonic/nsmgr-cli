7483f2fc8bb8f6c8f83fa7dba55812992f635217
Shah Z. S <shahzshafie@outlook.com>
2019-09-16 00:07:16 +0800

    #22 add LoggingException

---
+++ src/Nautilus.Cli.Core/Exceptions/LoggingException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nautilus.Cli.Core.Exceptions
+{
+	public class LoggingException : NullReferenceException
+	{
+		public LoggingException()
+		{
+		}
+
+		public LoggingException(string message) : base(message)
+		{
+		}
+
+		public LoggingException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
