e8d909927b376b626617f38202aca4a0938a031b
Shah Z. S <shahzshafie@outlook.com>
2019-09-16 00:04:03 +0800

    #22 add FileHelper in unit test project

---
+++ src/NautilusCli.Test/Helpers/FileHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace NautilusCliTest.Helpers
+{
+	public static class FileHelper
+	{
+		/// <summary>
+		/// Deletes the file given in the parameter
+		/// </summary>
+		/// <param name="fullFileName"></param>
+		/// <returns>Returns true if file is successful deleted, else returns false</returns>
+		internal static bool DeleteFile(string fullFileName)
+		{
+			if (File.Exists(fullFileName))
+			{
+				File.Delete(fullFileName);
+				return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Gets the desktop folder full filename
+		/// </summary>
+		/// <param name="logFileName"></param>
+		/// <returns></returns>
+		internal static string GetDesktopFullFileName(string logFileName)
+		{
+			var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			var fullFilePath = Path.Combine(desktopPath, logFileName);
+			return fullFilePath;
+		}
+
+	}
+}
