f26eb82b39333ee889787f94d5a451fd571a391b
Shah Z. S <shahzshafie@outlook.com>
2022-05-27 02:13:51 +0800

    #29 rename abstraction classes

--- src/Nautilus.SolutionExplorer.Core/Abstraction/IProjectFilePackageReader.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Nautilus.SolutionExplorer.Core.Abstraction
-{
-    public interface IProjectFilePackageReader
-    {
-        object Read(string fileName);
-    }
-}
---
+++ src/Nautilus.SolutionExplorer.Core/Abstraction/IProjectFileWriter.cs
@@ -0,0 +1,8 @@
+﻿namespace Nautilus.SolutionExplorer.Core.Abstraction;
+
+public interface IProjectFileWriter
+{
+    void Initialize(ProjectMetadata projectMetadata);
+    void UpdatePackageReference(string packageName, string newVersion);
+    void AddOrUpdateElement(string elementName, string value);
+}
