cb8a60f0a16f75a7ac2662fc0346ff998fca71dd
Shah Z. S <shahzshafie@outlook.com>
2022-09-29 16:43:35 +0800

    minor modification to method name

--- src/Nautilus.Cli.Client/AppStartup/Program.cs
+++ src/Nautilus.Cli.Client/AppStartup/Program.cs
@@ -4,8 +4,8 @@ Console.WriteLine("Nautilus solution manager cli");
 Console.WriteLine();
 
 var rootCommand = new RootCommand("Nautilus CLI for Visual Studio based solution manager");
-rootCommand.BuildPackageCommand();
-
+rootCommand.BuildNugetPackageCommand();
+rootCommand.BuildProjectCommand();
 
 //
 // project 
