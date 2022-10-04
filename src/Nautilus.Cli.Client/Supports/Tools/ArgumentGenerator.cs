0eea2918079a7365efb6bd12e5bd96ff9ec6d69e
Shah Z. S <shahzshafie@outlook.com>
2022-10-04 20:09:57 +0800

    impl ArgumentGenerator

---
+++ src/Nautilus.Cli.Client/Supports/Tools/ArgumentGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace Nautilus.Cli.Client.Supports.Tools;
+
+internal static class ArgumentGenerator
+{
+    internal static Argument<T> CreateArgument<T>(string name, string description, bool isHidden = false)
+    {
+        var argument = new Argument<T>
+        {
+            Name = name,
+            Description = description,
+            IsHidden = isHidden
+        };
+
+        return argument;
+    }
+
+    internal static CommonArgument? Common { get; private set; } = new CommonArgument();
+}
+
+internal class CommonArgument
+{
+    internal Argument<string> VersionArgument()
+    {
+        return ArgumentGenerator.CreateArgument<string>("version", "The version number to use");
+    }
+}
